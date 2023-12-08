b8df23fda62911c6241486973cd47a60a98e5762
John Dugan <34268+jdugan@users.noreply.github.com>
2023-12-08 08:06:13 +0100

    Day 8

--- src/day08/runner.py
+++ src/day08/runner.py
@@ -1,3 +1,6 @@
+import re
+from src.day08.node import Node
+from src.day08.simulator import Simulator
 from src.utility.reader import Reader
 
 class Day08:
@@ -9,10 +12,14 @@ class Day08:
         return 8
 
     def puzzle1(self):
-        return -1
+        simulation = self.__simulation()
+        steps      = simulation.fewest_steps(r'AAA', r'ZZZ')
+        return steps
 
     def puzzle2(self):
-        return -2
+        simulation = self.__simulation()
+        steps      = simulation.fewest_steps(r'\w\wA', r'\w\wZ')
+        return steps
 
 
     # -----------------------------------------------------
@@ -21,3 +28,14 @@ class Day08:
 
     def __data(_):
         return Reader().to_lines("data/day08/input.txt")
+
+    def __simulation(self):
+        lines   = self.__data()
+        instrs  = lines[0].strip()
+        network = {}
+        for line in lines[2:]:
+            match       = re.search(r'\A(\w+) = \((\w+), (\w+)\)\Z', line)
+            id, l, r    = match.groups()
+            node        = Node(id, l, r)
+            network[id] = node
+        return Simulator(instrs, network)
