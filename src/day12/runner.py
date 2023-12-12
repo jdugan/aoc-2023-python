65b3ae7017dfe918c168490f378b89ce1811263c
John Dugan <34268+jdugan@users.noreply.github.com>
2023-12-12 08:10:57 +0100

    Day 12

--- src/day12/runner.py
+++ src/day12/runner.py
@@ -1,3 +1,5 @@
+import time
+from src.day12.record import Record
 from src.utility.reader import Reader
 
 class Day12:
@@ -9,10 +11,14 @@ class Day12:
         return 12
 
     def puzzle1(self):
-        return -1
+        records = self.__records()
+        counts  = [r.possible_arrangements() for r in records]
+        return sum(counts)
 
     def puzzle2(self):
-        return -2
+        records = self.__expanded_records()
+        counts  = [r.possible_arrangements() for r in records]
+        return sum(counts)
 
 
     # -----------------------------------------------------
@@ -21,3 +27,30 @@ class Day12:
 
     def __data(_):
         return Reader().to_lines("data/day12/input.txt")
+
+    def __records(self):
+        lines   = self.__data()
+        records = []
+        for id, line in enumerate(lines):
+            springs, pattern = line.split()
+            pattern = [int(s) for s in pattern.split(",")]
+            record  = Record(id, springs, pattern)
+            records.append(record)
+        return records
+
+    def __expanded_records(self):
+        lines   = self.__data()
+        records = []
+        for id, line in enumerate(lines):
+            base_springs, base_pattern = line.split()
+            springs  = []
+            patterns = []
+            for _ in range(0, 5):
+                springs.append(base_springs)
+                patterns.append(base_pattern)
+            springs = "?".join(springs)
+            pattern = ",".join(patterns)
+            pattern = [int(s) for s in pattern.split(",")]
+            record  = Record(id, springs, pattern)
+            records.append(record)
+        return records
