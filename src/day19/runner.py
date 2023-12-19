22625737e4bca95bd03ed10ab054a30fe0f74ca5
John Dugan <34268+jdugan@users.noreply.github.com>
2023-12-19 07:43:09 +0100

    Day 19

--- src/day19/runner.py
+++ src/day19/runner.py
@@ -1,3 +1,6 @@
+import re
+from src.day19.part import Part
+from src.day19.sorter import Sorter
 from src.utility.reader import Reader
 
 class Day19:
@@ -9,10 +12,13 @@ class Day19:
         return 19
 
     def puzzle1(self):
-        return -1
+        sorter, parts = self.__data()
+        ratings       = [p.rating() for p in sorter.accepted_parts(parts)]
+        return sum(ratings)
 
     def puzzle2(self):
-        return -2
+        sorter, _ = self.__data()
+        return sorter.possible_parts()
 
 
     # -----------------------------------------------------
@@ -20,4 +26,33 @@ class Day19:
     # -----------------------------------------------------
 
     def __data(_):
-        return Reader().to_lines("data/day19/input.txt")
+        lines = Reader().to_lines("data/day19/input.txt")
+        rules = {}
+        parts = []
+        entry = lines[0].split("{")[0]
+        for line in lines:
+            if len(line) > 0:
+                if line[0] != "{":
+                    matches    = re.search(r"(\w+){(.+)}", line)
+                    key, cstrs = matches.groups()
+                    conditions = []
+                    for cstr in cstrs.split(","):
+                        if ":" in cstr:
+                            matches        = re.search(r"\A(\w+)([=><])(\d+):(\w+)\Z", cstr)
+                            a, op, b, dest = matches.groups()
+                            cond           = (a, op, int(b), dest)
+                            conditions.append(cond)
+                        else:
+                            cond = ("exit", "=", 1, cstr)
+                            conditions.append(cond)
+                    rules[key] = conditions
+                else:
+                    line    = line.replace("{", "").replace("}", "")
+                    pairs   = [s.split("=") for s in line.split(",")]
+                    ratings = { "exit": 1 }
+                    for k, v in pairs:
+                        ratings[k] = int(v)
+                    parts.append(Part(ratings))
+        sorter = Sorter(rules, entry)
+        return (sorter, parts)
+
